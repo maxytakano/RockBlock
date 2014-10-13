87feba9396275904732d5b45f76341eda184395f
Maximilien Takano <maxytakano@gmail.com>
2014-10-12 20:13:54 -0700

    test commit 2

--- RockBlock/RockBlock/GameScene.swift
+++ RockBlock/RockBlock/GameScene.swift
@@ -12,7 +12,7 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "freaking tim";
+        myLabel.text = "test yo";
         myLabel.fontSize = 25;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
